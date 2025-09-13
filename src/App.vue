20ba3d2edfad7fcb8b40c7d055621734ffc47ae9
matheus henry <goncalvessanches.mh@gmail.com>
2025-09-13 19:08:41 -0300

    fix: build

--- src/App.vue
+++ src/App.vue
@@ -366,7 +366,7 @@ const preventZoom = () => {
 
 // Função utilitária de debounce
 const debounce = (func: Function, wait: number) => {
-  let timeout: NodeJS.Timeout
+  let timeout: ReturnType<typeof setTimeout>
   return function executedFunction(...args: any[]) {
     const later = () => {
       clearTimeout(timeout)
